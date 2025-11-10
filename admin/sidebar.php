5752324176412b4312e0c0babeb7179f14376feb
Joswa24 <joshuapastorpide10@gmail.com>
2025-11-11 02:02:59 +0800

    commit

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -1,4 +1,7 @@
 <?php
+
+session_start();
+
 if (isset($_SESSION['success_message'])) {
     echo '<div class="alert alert-success">' . $_SESSION['success_message'] . '</div>';
     unset($_SESSION['success_message']);
@@ -16,7 +19,7 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true ||
 }
 // Include connection
 include '../connection.php';
-session_start();
+
 ?>
 <style>
     :root {
