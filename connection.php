0af18fb538a678997d33c7722f8aabb0ec41b414
Joswa24 <joshuapastorpide10@gmail.com>
2025-11-14 11:45:36 +0800

    awawa

--- connection.php
+++ connection.php
@@ -11,7 +11,7 @@ date_default_timezone_set('Asia/Manila');
 <?php
 //  date_default_timezone_set('Asia/Manila');
 
-//   $db = mysqli_connect('sql210.infinityfree.com','if0_39846067','HwDGHVhNuVvO','if0_39846067_gpassdb') or
+//   $db = mysqli_connect('sql210.infinityfree.com','if0_39846067','HwDGHVhNuVvO','if0_39846067_gpassdb')  or
 
 //         die ('Unable to connect. Check your connection parameters.');
 //         mysqli_select_db($db, 'if0_39846067_gpassdb' ) or die(mysqli_error($db));
